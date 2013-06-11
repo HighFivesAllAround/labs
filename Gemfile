a36ac9e6e543bf5fe6fd865a80dccc6185c91cb9
Tony Schneider <tonywok@gmail.com>
2013-06-10 23:25:17 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'active_model_serializers'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'ember-rails', :git => "git://github.com/emberjs/ember-rails.git", :branch => "master"
+gem 'ember-source', '1.0.0.rc5'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '4.0.0.rc1'
+gem 'sass-rails', '~> 4.0.0.rc1'
+gem 'uglifier', '>= 1.3.0'
+gem 'unicorn'
+
+group :development do
+  gem 'capistrano'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labs::Application.config.secret_key_base = '29e14a706f4bc868cfe0c1437d1132da30784a0123754fc8632d32435a4193ab51392fe2b14287b236b969977f2daed72891e9c3e70445177f0f754fd73825d1'
