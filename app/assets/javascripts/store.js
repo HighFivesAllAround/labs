30173c3f350c92dfe9cb8dda2c8d3644a838f0e8
Tony Schneider <tonywok@gmail.com>
2013-06-21 13:47:54 -0700

    Send csrf_token to satisfy forgery protection

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,3 +1,10 @@
+$(function() {
+  var token = $('meta[name="csrf-token"]').attr('content');
+  $.ajaxPrefilter(function(options, originalOptions, xhr) {
+    xhr.setRequestHeader('X-CSRF-Token', token);
+  });
+});
+
 DS.RESTAdapter.configure("Labs.Suggestion", { alias: "Suggestion" });
 DS.RESTAdapter.configure("Labs.Part", { alias: "Part" });
 
