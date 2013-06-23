89b6e4b0a435468afaa86e2569ff43899b19302d
Andrew Grieser <agrieser@gmail.com>
2013-06-23 16:18:08 -0700

    I don't think these are used anymore

--- app/assets/javascripts/controllers/projects_controller.js
+++
@@ -1,7 +0,0 @@
-Labs.ProjectsNewController = Ember.ObjectController.extend({
-  create: function() {
-    Labs.Store.POST("projects", this.content, $.proxy(function() {
-      this.transitionToRoute("projects.index");
-    }, this));
-  }
-});
