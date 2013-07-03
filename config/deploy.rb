495e439b0fc1b1bf8f19f2e9074796fdef0c953a
Tony Schneider <tonywok@gmail.com>
2013-07-02 23:43:35 -0700

    Restart unicorn on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,22 +17,27 @@ ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
 after "deploy", "deploy:cleanup"
-after 'deploy:cleanup', 'deploy:assets:cleanup'
+after "deploy", "deploy:restart"
+after "deploy:cleanup", "deploy:assets:cleanup"
 
 namespace :deploy do
-  task :setup_config, roles: :app do
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "sudo service app-#{application} restart"
+  end
+
+  task :setup_config, :roles => :app do
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
   end
   after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
+  task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
+  task :check_revision, :roles => :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
