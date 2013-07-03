1446294ab17b01ce2e692c2ea8e2513ec40db2f0
Andrew Grieser <agrieser@gmail.com>
2013-07-02 21:45:34 -0700

    Add upstart file for unicorn

--- vagrant/cookbooks/dick_and_jane/recipes/default.rb
+++ vagrant/cookbooks/dick_and_jane/recipes/default.rb
@@ -15,6 +15,13 @@ cookbook_file "/home/ubuntu/dick_and_jane/config/database.yml" do
   action :create_if_missing
 end
 
+cookbook_file "/etc/init/app-labs.conf" do
+  mode 0644
+  owner "root"
+  group "root"
+  action :create_if_missing
+end
+
 bash "create db" do
   cwd "/home/ubuntu/dick_and_jane"
   user "ubuntu"
