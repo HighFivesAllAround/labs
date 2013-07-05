e384d965e65c46e12f6d4d1a91ca0ec896df189d
Tony Schneider <tonywok@gmail.com>
2013-07-05 14:11:26 -0700

    Link directly to comment anchor - no ui yet

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -1,4 +1,8 @@
 Labs.Comment = DS.Model.extend({
   content: DS.attr("string"),
-  commentable: DS.belongsTo("Labs.Commentable", { polymorphic: true })
+  commentable: DS.belongsTo("Labs.Commentable", { polymorphic: true }),
+
+  anchor: function() {
+    return "comment-" + this.get("id");
+  }.property()
 });
