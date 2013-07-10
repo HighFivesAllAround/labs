fa8fb14e478cf9f57265ce952c97c899cbae3d32
Andrew Grieser <agrieser@gmail.com>
2013-07-09 21:26:07 -0700

    Stop including posts with project

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,6 +2,10 @@ class Api::PostsController < ApplicationController
 
   respond_to :json
 
+  def index
+    respond_with(:api, Project.find(params.require(:project_id)).posts.page(params[:page] || 1).per(params[:per_page] || 10))
+  end
+
   def create
     project = Project.find(params[:post].delete(:project_id))
     respond_with(:api, project.posts.create(post_params))
