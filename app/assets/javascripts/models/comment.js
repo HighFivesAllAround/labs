a2df2a2c1349e3f797c8a356a03141b67ceb1343
Andrew Grieser <agrieser@gmail.com>
2013-07-09 22:48:50 -0700

    Lint

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -5,7 +5,7 @@ Labs.Comment = DS.Model.extend({
   commentable: DS.belongsTo("Labs.Commentable", { polymorphic: true }),
 
   timeAgo: function() {
-    return moment(this.get("createdAt")).fromNow();
+    return window.moment(this.get("createdAt")).fromNow();
   }.property(),
 
   domId: function() {
