6cecda9e09f9ff8efc172f318b3081549ce6c03a
Tony Schneider <tonywok@gmail.com>
2013-07-16 01:21:07 -0700

    Remove cache in favor of 1-time observer
    Get metadata once, if they cancel, assume they want a regular text post.

--- app/assets/javascripts/views/new_post_view.js
+++ app/assets/javascripts/views/new_post_view.js
@@ -2,17 +2,19 @@ Labs.NewPostView = Ember.View.extend({
 
   templateName: "posts/new",
   classNames: ["row"],
+
   postBody: "",
-  isPreviewing: false,
+
   title: null,
   description: null,
   originalUrl: null,
   thumbnailUrl: null,
   html: null,
+
   urlRegexp: new RegExp(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i),
   tid: null,
   inFlight: false,
-  cache: {},
+  isPreviewing: false,
 
   submit: function(e) {
     e.preventDefault();
@@ -40,36 +42,42 @@ Labs.NewPostView = Ember.View.extend({
   }.observes("postBody"),
 
   fetchPreviewableContent: function(embedUrl) {
-    var cachedData = this.get("cache")[embedUrl];
-    if (cachedData) {
-      this.setProperties(this.extractMetadata(cachedData));
-    } else {
-      var self = this;
-      this.set("isPreviewing", true);
-      this.set("inFlight", true);
-      $.embedly.oembed(embedUrl, {key: 'f3a6ced6ff1e4b9b964d8a6a73088200'}).progress(function(data) {
-        self.set("inFlight", false);
-        self.get("cache")[embedUrl] = data;
-        self.setProperties(self.extractMetadata(data))
+    var self = this;
+    this.set("isPreviewing", true);
+    this.set("inFlight", true);
+    $.embedly.oembed(embedUrl, {key: 'f3a6ced6ff1e4b9b964d8a6a73088200'}).progress(function(data) {
+      self.set("inFlight", false);
+      self.removeObserver("postBody", "previewUrlObserver");
+      self.setProperties({
+        title: data.title,
+        description: data.description,
+        originalUrl: data.original_url,
+        thumbnailUrl: data.thumbnail_url,
+        html: data.html
       });
-    }
-  },
-
-  extractMetadata: function(data) {
-    return {
-      title: data.title,
-      description: data.description,
-      originalUrl: data.original_url,
-      thumbnailUrl: data.thumbnail_url,
-      html: data.html
-    }
+    });
   },
 
   postPreview: Ember.View.extend({
 
     classNames: ["row", "post-preview"],
-    isVisible: false,
     spinner: new Spinner(),
+
+    isVisible: function() {
+      return this.get("parentView.isPreviewing");
+    }.property("parentView.isPreviewing"),
+
+    cancel: function() {
+      this.set("parentView.isPreviewing", false);
+      this.get("parentView").setProperties({
+        title: null,
+        description: null,
+        originalUrl: null,
+        thumbnailUrl: null,
+        html: null
+      });
+      this.removeFromParent();
+    },
 
     loading: function() {
       var spinner = this.get("spinner");
@@ -79,24 +87,5 @@ Labs.NewPostView = Ember.View.extend({
         spinner.stop();
       }
     }.observes("parentView.inFlight"),
-
-    cancel: function() {
-      this.set("parentView.isPreviewing", false);
-    },
-
-    showOnce: function() {
-      if (this.get("parentView.isPreviewing")) {
-        this.set("isVisible", true);
-      } else {
-        this.set("parentView").setProperties({
-          title: null,
-          description: null,
-          originalUrl: null,
-          thumbnailUrl: null,
-          html: null
-        });
-        this.removeFromParent();
-      }
-    }.observes("parentView.isPreviewing")
   })
 });
